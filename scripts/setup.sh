2cb13a80c7294339e5b30482ad73a553a89f9a67
charlesoliveira-trab <charlesoliveira.trabalho@gmail.com>
2024-10-18 17:45:57 -0300

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -100,19 +100,19 @@ cat <<EOL >>scripts/linter.sh
 WIN_PATH='./'
 LINUX_PATH='./'
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+if [[ "\$OSTYPE" == "linux-gnu"* ]]; then
     # Linux
-    PROJECT_PATH=$LINUX_PATH
-    cd $PROJECT_PATH
-    echo $(pwd)
+    PROJECT_PATH=\$LINUX_PATH
+    cd \$PROJECT_PATH
+    echo \$(pwd)
     source .venv/bin/activate
     isort . && blue .
     deactivate
-elif [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "win32" ]]; then
+elif [[ "\$OSTYPE" == "cygwin" ]] || [[ "\$OSTYPE" == "msys" ]] || [[ "\$OSTYPE" == "win32" ]]; then
     # Windows
-    PROJECT_PATH=$WIN_PATH
-    cd $PROJECT_PATH
-    echo $(pwd)
+    PROJECT_PATH=\$WIN_PATH
+    cd \$PROJECT_PATH
+    echo \$(pwd)
     source .venv/Scripts/activate
     isort . && blue .
     deactivate
