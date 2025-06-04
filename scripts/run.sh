4dbe7e804cd72de7c5a74cb90fee09a80f8895a8
Charles Oliveira <neo_charles@outlook.com>
2025-06-04 20:12:22 -0300

    [update] run and .gitignore file updated

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,22 +1,102 @@
 #!/bin/bash
+#scripts/run.sh
 
-WIN_PATH='./'
-LINUX_PATH='./'
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+APP_PATH="$(dirname "$SCRIPT_DIR")"
+LOG_FILE="./logs/run.log"
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    # Linux
-    PROJECT_PATH=$LINUX_PATH
-    cd $PROJECT_PATH
-    echo $(pwd)
-    source .venv/bin/activate
-    python src/main.py 2>&1 | tee -a logs/main-$(date +"%Y%m%d-%H%M").log
+# Bash
+MAIN_FILE="./src/big_numbers.sh"
+WIN_CMD="bash"
+LINUX_CMD="bash"
+
+# Python:
+# MAIN_FILE="./src/main.py"
+# WIN_CMD="python"
+# LINUX_CMD="python3"
+
+# Java:
+# MAIN_FILE="./src/Main.java"
+# WIN_CMD="java"
+# LINUX_CMD="java"
+# LINUX_CMD="java -jar"
+
+# C#:
+# MAIN_FILE="./src/Program.cs
+# WIN_CMD="dotnet"
+# LINUX_CMD="dotnet"
+# LINUX_CMD="dotnet run"
+
+# C++:
+# MAIN_FILE="./src/main.cpp"
+# WIN_CMD="g++"
+# LINUX_CMD="g++"
+# LINUX_CMD="g++ -o main main.cpp"
+
+# javascript:
+# MAIN_FILE="./src/main.js"
+# WIN_CMD="node"
+# LINUX_CMD="node"
+
+
+
+# Função para exibir mensagens de erro e sair
+handle_error() {
+    echo "❌ Erro: $1"
     deactivate
-elif [[ "$OSTYPE" == "cygwin" ]] || [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "win32" ]]; then
-    # Windows
-    PROJECT_PATH=$WIN_PATH
-    cd $PROJECT_PATH
-    echo $(pwd)
-    source .venv/Scripts/activate
-    python src/main.py 2>&1 | tee -a logs/main-$(date +"%Y%m%d-%H%M").log
-    deactivate
-fi
+    exit 1
+}
+
+# Função principal para carregar variáveis do .env
+load_env() {
+    # Carrega as variáveis do arquivo .env
+    if [ ! -f .env ]; then
+        handle_error "Arquivo .env não encontrado."
+    fi
+
+    set -o allexport
+    source "$APP_PATH/.env"
+    set +o allexport
+    
+    echo "Arquivo .env carregado com sucesso!"
+}
+
+# Função principal para detectar o sistema operacional
+os_detect() {
+    OS=$(uname)
+    if [[ "$OS" == "Linux" ]]; then       
+        COMMAND=$LINUX_CMD
+    elif [[ "$OS" == CYGWIN* ]] || [[ "$OS" == MINGW* ]] || [[ "$OS" == "Windows"* ]]; then
+        COMMAND=$WIN_CMD
+    else
+        handle_error "Sistema operacional não suportado: $OS"
+    fi
+
+}
+
+# Função principal para executar o script Python
+run(){
+
+    echo "Navegando para o diretório do projeto..."
+    cd "$APP_PATH" || handle_error "Diretório do projeto não encontrado: $APP_PATH"
+    echo "Diretório atual: $(pwd)
+    "
+    
+    echo "Detectando sistema Operacional..."
+    os_detect
+    echo "Sistema Operacional: $OS
+    "
+
+    echo "Executando o script..."
+    $COMMAND $MAIN_FILE 2>&1 | tee -a $LOG_FILE
+    if [ $? -ne 0 ]; then
+        handle_error "Erro durante a execução do script."
+    fi
+}
+
+# Executa a função principal
+echo "Iniciando Processo..."
+echo ""
+run
+echo ""
+echo "Script executado com sucesso!"
